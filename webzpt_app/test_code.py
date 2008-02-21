079605752534dbb768c43bdadcd52007ebb7eff5
Pierre Larochelle <pierre.larochelle@gatech.edu>
2008-02-21 00:18:55 -0500

    changed test_code to show use of "repeat" operator of ZPT.

--- webzpt_app/test_code.py
+++ webzpt_app/test_code.py
@@ -1,13 +1,14 @@
-from webzpt import WebZPTController, web
+from webzpt import WebZPTController, web, class_from
 import re
 
 urls = (
     '/([a-zA-Z]+)', 'index')
-
+#authenticate\t900123456\ta
 class index(WebZPTController):
     def GET(self, name):
         self.name = name
-        print self.view(partials = (body(), top()))
+        pierre = student(firstname = 'Pierre', lastname = 'Larochelle')
+        print self.view(partials = (top(), body(), pierre))
 
 class body(WebZPTController):
     def content(self): 
@@ -16,6 +17,13 @@ class body(WebZPTController):
 class top(WebZPTController):
     def name(self):
         return 'Pierre Larochelle'
+
+class student(WebZPTController):
+    def __init__(self, firstname = 'empty', lastname = 'empty'):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.names = (firstname, lastname)
+
     
 
 web.webapi.internalerror = web.debugerror
--- webzpt_app/webzpt.py
+++ webzpt_app/webzpt.py
@@ -9,6 +9,7 @@ class PageTemplate(PageTemplate):
       return self.pt_render(extra_context=context)
 
 views_dir = curdir + sep + 'views' + sep
+defined_controllers = {}
 
 class WebZPTController:
     '''
@@ -16,25 +17,38 @@ class WebZPTController:
     application that wants to use Zope Page Templates
     '''
     template = PageTemplate()
-
+    
     def view(self, context = {}, partials = ()):
        '''
        Every controller will contain a view that displays it's
        zpt file in the views folder.
        '''
        if not partials == ():
-          for partial in partials:
-             context[partial.__class__.__name__] = partial
+          if type(partials) == tuple:
+             for partial in partials:
+                context[partial.__class__.__name__] = partial
+          else:
+             context[partials.__class__.__name__] = partials
           context['here'] = self
        else:
           if context == {}:
              context = {self.__class__.__name__:self}
           else:
-             context = {'here':self}
+             context['here'] = self
           
-       content = self.as_partial()
+       content = self.view_content()
        self.template.write(content)
-       return self.template(context = context)
+       #try:
+       template = self.template(context=context)
+       #except KeyError, keys:
+       #   for string in keys:
+       #      if globals().has_key(string):
+       #         partial_class = globals()[string]
+       #         context[string] = new_partial
+       #      else:
+       #         raise PartialNotFoundError, "No partial named '%s'" % globals()
+       #   template = self.template(context=context)
+       return template
 
     def GET(self):
        '''
@@ -47,9 +61,20 @@ class WebZPTController:
        return open(views_dir + self.__class__.__name__ + '.zpt')
           
 
-    def as_partial(self):
+    def view_content(self):
        try:
           file = self._view_file()
           return file.read()
        except IOError:
-          return "<html><body>Please create '%s.zpt' in '%s' before calling it's controller or using it as a partial" % (self.__class__.__name__, views_dir)
+          return "Please create '%s.zpt' in '%s' before calling it's controller or using it as a partial" % (self.__class__.__name__, views_dir)
+
+
+class PartialNotFoundError(Exception):
+   pass
+
+#Helper methods
+def class_from(string):
+   if globals().has_key(string):
+      return globals()#[string]
+   else:
+      return None
