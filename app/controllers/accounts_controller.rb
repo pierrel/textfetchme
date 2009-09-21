22dff871764ce2f4689f72b799b8b73d3a1d6590
Pierre Larochelle <pierre.larochelle@gatech.edu>
2009-09-21 11:39:09 -0400

    removed notice flash and changed error div color.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -15,6 +15,7 @@ class AccountsController < ApplicationController
         self.current_user.remember_me
         cookies[:auth_token] = { :value => self.current_user.remember_token , :expires => self.current_user.remember_token_expires_at }
       end
+      flash[:error] = "Incorrect username or password"
       redirect_back_or_default(:controller => '/triggers')
     end
   end
